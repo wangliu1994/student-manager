404092e85ea39382cf5988bf66f3648ccb18adb6
winnie <wangliu023@qq.com>
2019-04-04 10:55:55 +0800

    sync

--- student-service/src/main/java/com/winnie/student/ConvertConfig.java
+++ student-service/src/main/java/com/winnie/student/ConvertConfig.java
@@ -1,32 +1,26 @@
 package com.winnie.student;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * description web配置
+ * @author : winnie
+ * @date : 2019/4/4
+ * @desc
  */
 @Configuration
-public class ConvertConfig {
-
-    /**
-     * stringHttpMessageConverter
-     * @return StringHttpMessageConverter
-     */
+public class ConvertConfig extends WebMvcConfigurationSupport {
 //    @Bean
 //    public StringHttpMessageConverter stringHttpMessageConverter() {
 //        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter();
-//        List<MediaType> supportedMediaTypes = new ArrayList();
+//        List<MediaType> supportedMediaTypes = new ArrayList<>();
 //        supportedMediaTypes.add(MediaType.parseMediaType("text/html;charset=UTF-8"));
 //        supportedMediaTypes.add(MediaType.parseMediaType("text/plain;charset=UTF-8"));
 //        supportedMediaTypes.add(MediaType.parseMediaType("application/json;charset=UTF-8"));
@@ -34,18 +28,12 @@ public class ConvertConfig {
 //        return stringHttpMessageConverter;
 //    }
 
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        // 1.定义一个converters转换消息的对象
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-        // 2.添加fastjson的配置信息，比如: 是否需要格式化返回的json数据
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        // 3.在converter中添加配置信息
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-        // 4.将converter赋值给HttpMessageConverter
-        HttpMessageConverter<?> converter = fastConverter;
-        // 5.返回HttpMessageConverters对象
-        return new HttpMessageConverters(converter);
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
     }
 }
--- teacher-service/src/main/java/com/winnie/teacher/ConvertConfig.java
+++ teacher-service/src/main/java/com/winnie/teacher/ConvertConfig.java
@@ -15,10 +15,16 @@ import org.springframework.http.converter.HttpMessageConverter;
 public class ConvertConfig {
     @Bean
     public HttpMessageConverters fastJsonHttpMessageConverters() {
+        // 1.定义一个converters转换消息的对象
         FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        // 2.添加fastjson的配置信息，比如: 是否需要格式化返回的json数据
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        // 3.在converter中添加配置信息
         fastConverter.setFastJsonConfig(fastJsonConfig);
-        return new HttpMessageConverters((HttpMessageConverter<?>) fastConverter);
+        // 4.将converter赋值给HttpMessageConverter
+        HttpMessageConverter<?> converter = fastConverter;
+        // 5.返回HttpMessageConverters对象
+        return new HttpMessageConverters(converter);
     }
 }
